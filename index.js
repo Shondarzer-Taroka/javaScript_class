91932b81db2764cdb4965b17ccdd1894d014afeb
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-05-21 10:27:19 +0600

    static usecases done

--- index.js
+++ index.js
@@ -6,11 +6,61 @@ class Animal {
         this.sound = sound
     }
 
-    getAnimal(){
+    getAnimal() {
         console.log(`This ${this.name} sounds as ${this.sound}`);
-        
+
     }
 }
 
-const dog= new Animal('dog','gew gew')
+const dog = new Animal('dog', 'gew gew')
 console.log(dog.getAnimal());
+
+
+
+class Bird extends Animal {
+    constructor(name, sound, canFly) {
+        super(name, sound)
+        this.canFly = canFly
+    }
+
+    getBirdFly() {
+        if (this.canFly) {
+
+            console.log(`This bird ${this.name} can fly`);
+            // return `This bird ${this.name} can fly`
+        }
+        else {
+            console.log(` This bird ${this.name} cannot fly`);
+            // return ` This bird ${this.name} cannot fly`
+        }
+
+    }
+}
+
+const parrot = new Bird('Tiya', 'gr gri', true)
+console.log(parrot.getAnimal());
+console.log(parrot.getBirdFly());
+
+
+
+
+
+
+
+
+
+class MathUtils{
+    static add(a,b){
+        return a + b
+    }
+}
+
+let add=  MathUtils.add(23,345)
+console.log(add);
+
+
+
+
+
+
+
