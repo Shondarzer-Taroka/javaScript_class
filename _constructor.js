8adf77dd67b9099c8187e3b4c4e8710c8c7a46d6
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-05-21 14:02:11 +0600

    constructor methods and why methods are declared outside of its

---
+++ _constructor.js
@@ -0,0 +1,36 @@
+
+class Room {
+    constructor(name) {
+        this.name = name
+        this.wok = () => console.log(this.name);
+
+    }
+}
+
+
+
+class Room2{
+    constructor(name) {
+        this.name=name
+    }
+
+    getRoom(){
+        console.log(this.name);
+        
+    }
+}
+
+let room1=new Room('Roie')
+let room2= new Room('Weoi')
+room1.wok()
+room2.wok()
+console.log(room1);
+
+console.log(room1.wok===room2.wok);
+
+
+let room3=new Room2('Raopn')
+let room4=new Room2('Qeaui')
+console.log(room3.getRoom===room4.getRoom);
+console.log(room4);
+
